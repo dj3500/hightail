7b6d452390b2ac9cdf2024715fd93aa98aa67bd5
Jakub Tarnawski <dj3500@gmail.com>
2014-07-22 14:45:10 +0200

    Overhaul of the CodeForces task parser. Fixes #57.

--- Hightail/src/org/hightail/parsers/task/CodeForcesTaskParser.java
+++ Hightail/src/org/hightail/parsers/task/CodeForcesTaskParser.java
@@ -1,15 +1,19 @@
 package org.hightail.parsers.task;
 
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import org.hightail.Problem;
 import org.hightail.SupportedSites;
 import org.hightail.Testcase;
 import org.hightail.TestcaseSet;
+import org.htmlparser.Node;
 import org.htmlparser.NodeFilter;
 import org.htmlparser.beans.FilterBean;
 import org.htmlparser.filters.CssSelectorNodeFilter;
-import org.htmlparser.filters.HasParentFilter;
+import org.htmlparser.nodes.TagNode;
+import org.htmlparser.nodes.TextNode;
+import org.htmlparser.util.NodeList;
 import org.htmlparser.util.ParserException;
 import org.htmlparser.util.Translate;
 
@@ -18,10 +22,53 @@ import org.htmlparser.util.Translate;
  * @author robertrosolek
  */
 public class CodeForcesTaskParser implements TaskParser {
+    
+    private ArrayList<String> extractInputsOrOutputs (FilterBean fb, String cssSelector) throws ParserException {
+        NodeFilter filter = new CssSelectorNodeFilter(cssSelector);
+        fb.setFilters(new NodeFilter[] {filter});
+        NodeList nodeList = fb.getNodes();
+        if (nodeList == null || nodeList.size() == 0) {
+            throw new ParserException("Parsing failed - no testcases were extracted.");
+        }
+        Node[] nodes = nodeList.toNodeArray();
+        ArrayList<String> inputs = new ArrayList<>();
+        for (Node n : nodes) {
+            /* The children of each node look like this:
+             * div class="title"
+             * pre
+             * 3 4 1
+             * br /
+             * 1 2 3
+             * br /
+             * 2 3 5
+             * br /
+             * /pre
+             */
+            NodeList childrenNodeList = n.getChildren();
+            if (childrenNodeList == null) {
+                throw new ParserException("Parsing failed.");
+            }
+            Node[] children = childrenNodeList.toNodeArray();
+            StringBuilder contents = new StringBuilder();
+            for (Node child : children) {
+                if (child instanceof TextNode) {
+                    contents.append(Translate.decode(child.getText()));
+                } else if (child instanceof TagNode && child.getText().contains("br")) {
+                    contents.append("\n");
+                }
+            }
+            String contentsString = contents.toString();
+            if (contentsString.isEmpty()) {
+                throw new ParserException("Parsing failed.");
+            } 
+            inputs.add(contentsString);
+        }
+        return inputs;
+    }
 
     @Override
     public Problem parse(String URL) throws ParserException, InterruptedException {
-        
+
         URL = URL.trim();
         
         FilterBean fb = new FilterBean();
@@ -42,18 +89,16 @@ public class CodeForcesTaskParser implements TaskParser {
         }
         
         // extract inputs
-        CssSelectorNodeFilter inputFilter = new CssSelectorNodeFilter("div.input");
-        HasParentFilter preInputFilter = new HasParentFilter(inputFilter);
-        fb.setFilters(new NodeFilter[] {preInputFilter});
-        String[] inputs = Translate.decode(fb.getText()).split("Input");
+        ArrayList<String> inputs = extractInputsOrOutputs(fb, "div.input");
+
+        if (Thread.interrupted()) {
+            throw new InterruptedException();
+        }
         
         // extract outputs
-        CssSelectorNodeFilter outputFilter = new CssSelectorNodeFilter("div.output");
-        HasParentFilter preOutputFilter = new HasParentFilter(outputFilter);
-        fb.setFilters(new NodeFilter[] {preOutputFilter});
-        String[] outputs = Translate.decode(fb.getText()).split("Output");
+        ArrayList<String> outputs = extractInputsOrOutputs(fb, "div.output");
         
-        if (inputs.length != outputs.length) {
+        if (inputs.size() != outputs.size()) {
             throw new ParserException("Number of inputs is not equal to number of outputs.");
         }
         
@@ -73,8 +118,8 @@ public class CodeForcesTaskParser implements TaskParser {
         
         
         TestcaseSet testcaseSet = new TestcaseSet();
-        for (int i = 1; i < inputs.length; ++i) {
-            testcaseSet.add(new Testcase(inputs[i].trim(), outputs[i].trim(), timeLimit));
+        for (int i = 0; i < inputs.size(); ++i) {
+            testcaseSet.add(new Testcase(inputs.get(i).trim(), outputs.get(i).trim(), timeLimit));
         }
         
         if (Thread.interrupted()) {
